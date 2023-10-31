05f15d3962605c49852f3b7cc417494c8904623f
Anna Ostrowska <ana.os@wp.pl>
2023-10-31 19:40:15 +0100

    Part 1 completed

--- app.js
+++ app.js
@@ -21,6 +21,22 @@ class PianoRollDisplay {
   preparePianoRollCard(rollId) {
     const cardDiv = document.createElement('div');
     cardDiv.classList.add('piano-roll-card');
+    cardDiv.id = 'piano-roll-card-' + rollId;
+    cardDiv.setAttribute('roll-id', rollId);
+
+    // Handle PianoRoll selection
+    cardDiv.addEventListener("click", function() {
+      console.log(selectedRollId);
+      if(selectedRollId != rollId) {
+        selectedRollId = rollId;
+        setMainView();
+      }
+      else {
+        selectedRollId = null;
+        setGridView();
+      }
+      console.log(selectedRollId);
+    });
 
     // Create and append other elements to the card container as needed
     const descriptionDiv = document.createElement('div');
@@ -62,3 +78,57 @@ document.getElementById('loadCSV').addEventListener('click', async () => {
   const csvToSVG = new PianoRollDisplay();
   await csvToSVG.generateSVGs();
 });
+
+// Global variables
+var selectedRollId = null;
+var selectedCardIdPrev = null;
+let pianoRollContainer = null;
+let viewContainer = null;
+let mainPianoRollContainer = null;
+
+// DOM elements
+window.addEventListener('load', () => {
+  pianoRollContainer = document.getElementById('pianoRollContainer');
+  viewContainer = document.getElementById('viewContainer');
+  mainPianoRollContainer = document.getElementById('mainPianoRollContainer');
+});
+
+// Remove PianoRoll from Main View
+function clearCardSelection() {
+  const prevCard = document.getElementById(selectedCardIdPrev);
+  pianoRollContainer.appendChild(prevCard);
+  selectedCardIdPrev = null;
+}
+
+// Change view to Main
+function setMainView() {
+  pianoRollContainer.classList.add('mainView');
+  pianoRollContainer.classList.remove('gridView');
+
+  viewContainer.classList.add('mainViewContainer');
+  viewContainer.classList.remove('gridViewContainer');
+
+  if(selectedCardIdPrev != null) {
+    clearCardSelection();
+  }
+  // Add PianoRoll to Main View
+  const selectedCardId = 'piano-roll-card-' + selectedRollId;
+  const selectedCard = document.getElementById(selectedCardId);
+  mainPianoRollContainer.appendChild(selectedCard);
+  selectedCardIdPrev = selectedCardId;
+}
+
+// Chnage view to Grid
+function setGridView() {
+  pianoRollContainer.classList.add('gridView');
+  pianoRollContainer.classList.remove('mainView');
+
+  viewContainer.classList.add('gridViewContainer');
+  viewContainer.classList.remove('mainViewContainer');
+
+  if(selectedCardIdPrev != null) {
+    clearCardSelection();
+  }
+}
+
+
